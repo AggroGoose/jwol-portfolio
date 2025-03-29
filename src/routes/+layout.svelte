51ac10cec2b216e1084967479597ef783aa48e7c
StoicGoose <josh@jwolmedia.com>
2025-03-29 14:31:57 -0600

    Converted SVGs to Svelte Components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,11 @@
 	let { children } = $props();
 </script>
 
-<main class="bg-tn-bg min-h-screen">
+<svelte:head>
+	<title>Josh Walter Portfolio</title>
+	<meta name="description" content="Developer portfolio for Josh Walter" />
+</svelte:head>
+
+<main class="bg-tn-bg min-h-screen max-w-screen">
 	{@render children()}
 </main>
---
+++ src/routes/sections/SVG/VercelIcon.svelte
@@ -0,0 +1,7 @@
+<script>
+	let { className } = $props();
+</script>
+
+<svg class={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path d="M12 1L24 22H0L12 1Z" fill="#000000" />
+</svg>
